194c2af37413583dfc156884ac446e584a345438
NuralySol <soltonbekov@gmail.com>
2024-09-24 17:48:49 -0400

    Refactor build configuration and update outDir in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist', // This is the default, so if it's already 'dist', you can omit this.
+    outDir: 'dist', 
   },
 })
